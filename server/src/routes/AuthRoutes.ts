5fcc716c3f5105c48fe123206e6e4e5963d88a9f
yasho11 <yyashodeep.basnet@gmail.com>
2025-02-15 20:38:55 +0545

    Added getuserprofile and updateuser

--- server/src/routes/AuthRoutes.ts
+++ server/src/routes/AuthRoutes.ts
@@ -6,13 +6,19 @@ import {
   UpdateProfile,
 } from "../controllers/AuthController";
 import multer from "multer";
+import { verifyToken } from "../middleware/verifytoken";
 
 const router = Router();
 const upload = multer({ dest: "public/uploads" });
 
-router.post("/register",upload.single("ProfileURL"),register);
+router.post("/register", upload.single("ProfileURL"), register);
 router.post("/login", login);
-router.get("/me", getCurrentUser);
-router.put("/update-profile", UpdateProfile);
+router.get("/me", verifyToken, getCurrentUser);
+router.put(
+  "/update-profile",
+  verifyToken,
+  upload.single("ProfileURL"),
+  UpdateProfile
+);
 
 export default router;
